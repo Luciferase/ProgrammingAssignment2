42f7e49e14a9d39f5888c4a85b9abd3955b45373
Luciferase <f.alex.jacobs@gmail.com>
2015-02-19 11:23:13 +0200

    cachematrix
    Error in cacheSolve(z) : attempt to apply non-function

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,15 @@
 ## makeCacheMatrix is a function that creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setSolve <- function(solve) m <<- solve
+        getSolve <- function() m
+        list(set = set, get = get, setSolve = setSolve, getmean = getSolve)
 }
 
 
@@ -18,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getSolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmean(m)
+        m
 }
